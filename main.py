ebdf85e3b2b4a79b4f960fe6dae997dc7358260a
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-12-18 09:54:24 +0545

    Migration

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def connect():
 
 def get_rows(con): 
    try:
-      with con.cursor() as cursor:
+         cursor = con.cursor() 
          cursor.execute('SELECT * FROM company')
          header = [item[0] for item in cursor.description]
          rows = cursor.fetchall()
+         print(header)
          return header, rows
    except:
       print("Error: unable to fecth data")
@@ -22,26 +23,24 @@ def get_value_pair(header, rows):
       result.append({header[i]: value for i, value in enumerate(row)})
    return result
 
-def update_rows(row):
+def update_rows(con,key_name):
    try:
-      with con.cursor() as cursor:
-         cursor.execute('''''')
-         # rows = cursor.fetchall()
-         return  rows
-   except:
-      print("Error: unable to fecth data")  
+         cursor = con.cursor() 
+         print(key_name,"\n")
+         # cursor.execute('UPDATE company SET {0} = CASE WHEN {0} = "" THEN NULL ELSE {0} END '.format(key_name))
+         return True
+   except Exception as exec:
+      print("Error: unable to fecth data",exec)  
 
 def main():
    con  = connect()
-   header, rows = get_rows(con)
-   dict_result = get_value_pair(header, rows)
+   header,rows = get_rows(con)
+   # dict_result = get_value_pair(header, rows)
    # print(dict_result)
-   for row in dict_result:
-      # print(row,row["co_id"],"\n")
-      for key in row:
-
-
-         print(key,"\n")
-
+   for key in header:
+      if key == "co_created_at" or key == "co_updated_at" or key == "co_report_link_url" or key == "co_additional_comment":
+         return True
+      else:
+         update_rows(con,key)   
+   con.close()
 main()
-
